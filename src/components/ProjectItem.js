0d7a9eae207341e06dd4505488568f4aaf2e4c4a
Anastasia Shatokhina <shnatya@gmail.com>
2021-11-18 12:28:29 -0800

    commit tree 78c27b71046bf940b58b211d05d18ca2930e5a61

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log("this is array:" + technologies)
+  console.log(technologies[0], technologies[1]) //{el}?
+  const techList = technologies.map(el => <span key = {el}>{el}</span>) //el key? but they repeat 
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techList}
       </div>
     </div>
   );
